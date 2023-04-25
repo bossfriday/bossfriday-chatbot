4e15c2f3ca6643202185cff0f11bb5cf1f8b389e
chenxiang <chenxiang@rongcloud.cn>
2023-04-25 14:40:17 +0800

    httpClient超时时间设置；

--- src/main/java/cn/bossfriday/chatbot/core/client/PoolingRestClient.java
+++ src/main/java/cn/bossfriday/chatbot/core/client/PoolingRestClient.java
@@ -6,6 +6,8 @@ import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * PoolingRestTemplate
  *
@@ -14,19 +16,34 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public abstract class PoolingRestClient<T> {
 
+    private static final int CONNECTION_TIME_TO_LIVE = 300 * 1000;
+    private static final int CONNECT_TIMEOUT = 30 * 1000;
+    private static final int READ_TIMEOUT = 60 * 1000;
+    private static final int NO_CONNECTION_WAITING = CONNECT_TIMEOUT + READ_TIMEOUT + 1000;
+
     protected String name;
     protected RestTemplate restTemplate;
     protected PoolingHttpClientConnectionManager connectionManager;
 
     protected PoolingRestClient(String name, int restClientMaxTotal, int restClientMaxPerRoute) {
         this.name = name;
+
+        // init connectionManager
         this.connectionManager = new PoolingHttpClientConnectionManager();
         this.connectionManager.setMaxTotal(restClientMaxTotal);
         this.connectionManager.setDefaultMaxPerRoute(restClientMaxPerRoute);
-        this.restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(HttpClientBuilder.create()
+
+        // init clientHttpRequestFactory
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(HttpClientBuilder.create()
                 .setConnectionManager(this.connectionManager)
-                .build()));
-        log.info("PoolingRestClient build done, name=" + this.name + ", maxTotal=" + this.connectionManager.getMaxTotal() + ", maxPerRoute=" + this.connectionManager.getDefaultMaxPerRoute());
+                .setConnectionTimeToLive(CONNECTION_TIME_TO_LIVE, TimeUnit.MILLISECONDS)
+                .build());
+        clientHttpRequestFactory.setConnectTimeout(CONNECT_TIMEOUT);
+        clientHttpRequestFactory.setReadTimeout(READ_TIMEOUT);
+        clientHttpRequestFactory.setConnectionRequestTimeout(NO_CONNECTION_WAITING);
+
+        this.restTemplate = new RestTemplate();
+        this.restTemplate.setRequestFactory(clientHttpRequestFactory);
     }
 
     /**
